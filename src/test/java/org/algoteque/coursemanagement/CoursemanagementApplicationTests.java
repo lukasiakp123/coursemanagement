3230b0f825b18b19fc68dd5711c68a4bb88009b5
Paweł Łukasiak (PWU) <PWU@kmd.dk>
2025-06-07 11:43:31 +0200

    [TASK-1] - initial commit

---
+++ src/test/java/org/algoteque/coursemanagement/CoursemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.algoteque.coursemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
